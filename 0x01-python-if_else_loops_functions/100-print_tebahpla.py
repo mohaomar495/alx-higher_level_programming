b2c171330941ed27e20afcf0362be86f3b5e859c
Mohammad Adde <mohaomar495@gmail.com>
2023-11-29 04:44:45 -0800

    python

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 i = 0
 for char in range(122, 96, -1):
     print(f"{chr(char - i)}", end="")
--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def remove_char_at(str, n):
     if len(str) > n:
         return (f"{str.replace(str[n], '')}")
