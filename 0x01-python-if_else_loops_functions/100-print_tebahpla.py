3e8c19b31d5c4c0586ed6c8d4391d041d36137be
Mohammad Adde <mohaomar495@gmail.com>
2023-11-29 05:00:51 -0800

    python

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 i = 0
 for char in range(122, 96, -1):
-    print(f"{chr(char - i)}", end="")
+    print(f"{}".format(char - i), end="")
     if i == 0:
         i = 32
     else:
