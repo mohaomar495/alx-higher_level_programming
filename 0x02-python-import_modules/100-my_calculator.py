b40229dfb14795c971c42b5125a5c9bdcede8d7a
Mohammad Adde <mohaomar495@gmail.com>
2023-11-30 11:55:45 -0800

    python

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv, exit
+
+if __name__ == "__main__":
+    if len(argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+    operators = {"+": add, "-": sub, "*": mul, "/": div}
+    if argv[2] not in operators.keys():
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
+    a = int(argv[1])
+    b = int(argv[3])
+    operator = argv[2]
+    print("{} {} {} = {}".format(a, operator, b, operators[operator](a, b)))
