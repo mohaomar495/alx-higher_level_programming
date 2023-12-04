b2b7e44e6350ace3d435959f3ce92fb343fb9706
Mohammad Adde <mohaomar495@gmail.com>
2023-12-04 03:47:35 -0800

    python

---
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def new_in_list(my_list, idx, element):
+    copied_list = my_list.copy()
+    if idx < 0 or idx > len(my_list) - 1:
+        return copied_list
+    copied_list[idx] = element
+    return copied_list
