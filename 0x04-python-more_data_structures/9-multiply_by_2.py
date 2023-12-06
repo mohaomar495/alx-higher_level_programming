58f8dbda442cfe08e214381e135b76cb5c6755e4
Mohammad Adde <mohaomar495@gmail.com>
2023-12-06 09:13:11 -0800

    python

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -2,6 +2,5 @@
 def multiply_by_2(a_dictionary):
     new_a_dictionary = a_dictionary.copy()
     for key in new_a_dictionary.keys():
-        if new_a_dictionary[key] >= 0 and new_a_dictionary[key] <= 9:
-            new_a_dictionary[key] = new_a_dictionary[key] * 2
+        new_a_dictionary[key] *= 2
     return new_a_dictionary
