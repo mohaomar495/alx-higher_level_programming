9975d31497f58d7ae182899160ad6180effaf987
Mohammad Adde <mohaomar495@gmail.com>
2024-02-12 00:47:24 -0800

    square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" square class """
+
+
+class Square:
+    """Square"""
+
+    def __init__(self, size):
+        """Initialize a new Square
+        args:
+            size(int): the size of the square
+        """
+        self.__size = size
