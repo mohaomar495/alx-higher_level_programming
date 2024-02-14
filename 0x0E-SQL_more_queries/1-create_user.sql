408852490f41a5945c85f1f8002e2e60741b4767
Mohammad Adde <mohaomar495@gmail.com>
2024-02-14 23:05:47 +0300

    more queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,3 @@
 -- script to creates the MYSQL server user user_0d_1
 
-CREATE USER 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
+CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED WITH mysql_native_password BY 'user_0d_1_pwd';
