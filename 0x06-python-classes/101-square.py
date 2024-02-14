565e972d6c55b889f28a8a9c496afc9274ab93e2
Mohammad Adde <mohaomar495@gmail.com>
2024-02-14 04:14:06 -0800

    square

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -52,6 +52,18 @@ class Square:
             raise TypeError("position must be a tuple of 2 positive integer")
         self.__position = value
 
+    def my_print(self):
+        """Print the square with the # character."""
+        if self.__size == 0:
+            print("")
+            return
+
+        [print("") for i in range(0, self.__position[1])]
+        for i in range(0, self.__size):
+            [print(" ", end="") for j in range(0, self.__position[0])]
+            [print("#", end="") for k in range(0, self.__size)]
+            print("")
+
     def __str__(self):
         if self.__size == 0:
             print("")
