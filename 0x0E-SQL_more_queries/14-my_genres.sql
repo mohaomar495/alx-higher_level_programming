a01f273e318f107aeeff2e73aa86991411502ed9
Mohammad Adde <mohaomar495@gmail.com>
2024-02-15 11:01:09 +0300

    more queries

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -6,4 +6,5 @@ ON tv_shows.id = tv_show_genres.show_id
 LEFT JOIN tv_genres
 ON tv_show_genres.genre_id = tv_genres.id
 WHERE tv_shows.title = 'Dexter'
+GROUP BY tv_genres.name
 ORDER BY tv_genres.name DESC;
