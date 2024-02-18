839c93188ed9106c68db250c4f2e7c06ff76d30f
Mohammad Adde <mohaomar495@gmail.com>
2024-02-18 02:59:39 -0800

    square

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -62,7 +62,7 @@ class Square:
     def __str__(self):
         """String representation of the square."""
         if self.__size != 0:
-            [print("", end="") for i in range(0, self.__position[1])]
+            [print("") for i in range(0, self.__position[1])]
         for i in range(0, self.__size):
             [print(" ", end="") for j in range(0, self.__position[0])]
             [print("#", end="") for k in range(0, self.__size)]
