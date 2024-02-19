9501f97a123ec0b7f28a928374b631120339e147
Mohammad Adde <mohaomar495@gmail.com>
2024-02-19 10:01:31 -0800

    test

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+def add_integer(a, b=98):
+    """
+        function that adds two integer or floats and returns the added values.
+
+        :param a: must be an integer or float
+        :param b: must be an integer or float
+        :return: returns an integer which is the addition of a and b
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    return round(a + b)
