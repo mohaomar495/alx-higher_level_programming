9fbfeb61da8e96dc532a737eb9f0ce2c75738cc7
mohaomar495 <mohaomar495@gmail.com>
2024-08-06 13:00:04 -0700

    testing matrix division

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -3,7 +3,10 @@
 
 
 def matrix_divided(matrix, div=1):
-    """Return a new matrix which is the result of the original matrix divided by div.
+    """
+    Return a new matrix which is the result
+    of the original matrix divided by div.
+
     :param matrix: matrix (list of lists) of integers/floats
     :param div: div should be integer/float and also it should not be zero.
     :return: new matrix divided by div.
@@ -17,7 +20,9 @@ def matrix_divided(matrix, div=1):
             for elem in [num for row in matrix for num in row]
         )
     ):
-        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+        raise TypeError(
+                "matrix must be a matrix (list of lists) of integers/floats"
+                )
     if not all(len(row) == len(matrix[0]) for row in matrix):
         raise TypeError("Each row of the matrix must have the same size")
     if not (isinstance(div, int) or isinstance(div, float)):
